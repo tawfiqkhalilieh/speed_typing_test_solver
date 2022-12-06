07a5404ab7f0cd7bb9fbd4a59ce9fd73914a9123
xtawfeeq7k <tawfeeqkhalilia51@gmail.com>
2022-12-06 21:15:57 +0200

    �

--- main.py
+++ main.py
@@ -8,19 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 browser = webdriver.Chrome("chromedriver.exe")
 browser.get("https://typing-speed-test.aoeu.eu/")
 
-input = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR,"[id='input']"))
-)
+input = WebDriverWait(browser, 10).until( EC.presence_of_element_located((By.CSS_SELECTOR,"[id='input']")))
 
 input.click()
-for i in range(300):
-    element = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "[id='currentword']"))
-    )
-    input.send_keys(element.text)
-    input.send_keys(" ")
-
-time.sleep(20)
-
+for i in range(300): input.send_keys(WebDriverWait(browser, 10).until( EC.presence_of_element_located((By.CSS_SELECTOR, "[id='currentword']")) ).text + ' ')
 print("\nSpeed typing finished successfully!\n")
+time.sleep(20)
 browser.quit()
